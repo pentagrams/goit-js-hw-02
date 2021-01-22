0094fbf187baf9a25dbb3a976bfa8685ec41de50
Max M <maxminullin@gmail.com>
2021-01-22 19:26:47 +0200

    addlasttasks

--- js/task-6.js
+++ js/task-6.js
@@ -1,3 +1,22 @@
 let input;
 const numbers = [];
 let total = 0;
+
+const addArray = function (number) {   
+   while(input !== null){
+        input = prompt();
+        numbers.push(Number(input));
+    }
+    return numbers;
+  }
+
+const sumArray = function (array) {   
+    for (let i = 0; i < numbers.length; i++){
+        total += numbers[i]
+    }
+    
+    return total;
+}
+addArray()
+sumArray(numbers)
+console.log(total)
